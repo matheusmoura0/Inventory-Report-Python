360777827e5317828cc704c147ba132cfbe5e64d
Matheus <matheusmoura2110@gmail.com>
2022-08-12 17:29:39 -0300

    feat: simplereport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,8 +1,18 @@
+from datetime import date
+
+
 class SimpleReport:
-    def __init__(self, product):
-        self.product = product
+    @staticmethod
+    def generate(inventory):
+        nome_da_empresa = [i["nome_da_empresa"] for i in inventory]
+        data_de_fabricacao = min([i["data_de_fabricacao"] for i in inventory])
+        data_de_validade = min([i["data_de_validade"] for i in inventory
+                                if i['data_de_validade'] >= str(date.today())])
+        empresa_com_mais_produtos = max(set(nome_da_empresa),
+                                        key=nome_da_empresa.count)
 
-
-def generate_report(product):
-    report = SimpleReport(product)
-    return report
+        return (
+            f"Data de fabricação mais antiga: {data_de_fabricacao}\n"
+            f"Data de validade mais próxima: {data_de_validade}\n"
+            f"Empresa com mais produtos: {empresa_com_mais_produtos}"
+        )
