cd3c93ab7fca40e190ba3ed1f9ab21a07a1fb283
Matheus <matheusmoura2110@gmail.com>
2022-08-12 16:03:40 -0300

    refactor test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -11,13 +11,6 @@ def test_relatorio_produto():
         "4521540540",
         "No bolso do ladino",
     )
-    assert produto.id == 1
-    assert produto.nome_do_produto == "Rapier"
-    assert produto.nome_da_empresa == "Dungeon"
-    assert produto.data_de_fabricacao == "01/01/2020"
-    assert produto.data_de_validade == "01/01/2021"
-    assert produto.numero_de_serie == "4521540540"
-    assert produto.instrucoes_de_armazenamento == "No bolso do ladino"
     assert produto.__repr__() == (
         "O produto Rapier"
         "fabricado em 01/01/2020 "
